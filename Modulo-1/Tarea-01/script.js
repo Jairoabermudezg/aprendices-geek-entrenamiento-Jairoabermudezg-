2ce735b1f49f7eae190494bc03e1b38442d31714
jairoabermudezg <jairoabermudezgu@gmail.com>
2022-03-22 19:06:14 -0500

    completo

---
+++ Modulo-1/Tarea-01/script.js
@@ -0,0 +1,16 @@
+function calcular(){
+    var pes = (document.convertidor.pesos.value) * 1;
+
+    var tot = pes / 16;
+
+    document.convertidor.dolar.value = tot;
+
+}
+
+function calcular(){
+    var pes = (document.convertidor.pesos.value) * 1;
+
+    var tot = pes / 16; 
+
+    document.convertidor.dolar.value = tot;
+}
